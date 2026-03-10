d5213ca46fdf48d5d331cab6bd0fd5c2717a065b
Lucas Ariel <lariel.olim@gmail.com>
2026-03-10 02:29:43 -0300

    add the map named asciiDescription which provides descriptions to values

--- ascci_table.go
+++ ascci_table.go
@@ -5,25 +5,24 @@ import (
 	// "github.com/olekukonko/tablewriter"
 )
 
-
-
-func getASCIIRelation(interval[2] int, extended bool, detailed bool) [][3]string { // mudar 3->4
-	toReturn := make([][3]string, interval[1]-interval[0]+1) // mudar 3->4
+func getASCIIRelation(interval[2] int) [][4]string { 
+	toReturn := make([][4]string, interval[1]-interval[0]+1) 
 
 	for i := range len(toReturn) {
 		currentDecimalValue := interval[0] + i
-		toReturn[i] = [3]string{
+		toReturn[i] = [4]string{
 			fmt.Sprintf("%d", 		currentDecimalValue), 		// decimal
 			fmt.Sprintf("%2x", 		currentDecimalValue), 		// hexadecimal
 			fmt.Sprintf("%s", string(currentDecimalValue)),		// symbol
-		} //mudar 3->4
+			fmt.Sprintf("%s", asciiDescription[currentDecimalValue]),
+		} 
 	}
 
 	return toReturn
 }
 
 func main() {
-	getASCIIRelation([2]int{32, 127}, false)
+	fmt.Println(getASCIIRelation([2]int{32, 127}))
 	fmt.Println("running")	
 }
 
