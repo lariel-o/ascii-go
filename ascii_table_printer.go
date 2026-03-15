05961d50253b4da85960c8c918bd013f80e91660
Lucas Ariel <lariel.olim@gmail.com>
2026-03-15 01:52:23 -0300

    remake

--- ascii_table_printer.go
+++ ascii_table_printer.go
@@ -1,6 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+	"math"
+)
+
 func main() {
-	
+	currentTableType := "printable"
+
+	if(len(os.Args) > 1) {
+		currentTableType = os.Args[1]
+	}
+
+	currentTableMin 		:= tableType[currentTableType][0]
+	currentTableMax 		:= tableType[currentTableType][1]
+	currentTableLength		:= currentTableMax - currentTableMin 
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', tabwriter.TabIndent)
+	header := "Dec\tHex\tSymb\t\tDec\tHex\tSymb\t\tDec\tHex\tSymb\t\tDec\tHex\tSymb\t\tDec\tHex\tSymb\t\tDec\tHex\tSymb\t\t"
+	fmt.Fprintln(w, header)
+
+	loopCount := int(math.Round(float64(currentTableLength) / 6))
+	baseLine := currentTableMin
+	for i := range loopCount {
+		if i != 0 && i % 8 == 0 {
+			w.Flush()
+			fmt.Fprintln(w, "\n" + header)
+			baseLine += 41
+		}
+
+		runningLine := baseLine + i 
+		line := ""
+
+
+		for j := range 6  {
+			currentDec := runningLine + j*8
+			currentHex := fmt.Sprintf("%x", currentDec)
+			currentSym := string(currentDec)
+
+			if currentTableMax < currentDec {
+				break
+			}
+
+			line += fmt.Sprintf("%d\t%s\t%s\t\t", currentDec, currentHex, currentSym)
+		}
+
+		fmt.Fprintln(w, line)
+	}
+
+	// talvez (?) precisa de um metodo para verificar se loopCount/8 é um inteiro
+	w.Flush()
 }
 
--- tables_type.go
+++ tables_type.go
@@ -1,6 +1,6 @@
 package main
 
-var tables_type = map[string][2]int{
+var tableType = map[string][2]int{
 	"printable": 		[2]int{32, 127},
 	"control":   		[2]int{0, 31},
 	"extended": 		[2]int{128, 255},
